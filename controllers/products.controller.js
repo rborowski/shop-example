18f0365220872a0b3b2d6403eb85d3c483fb20cf
Rafał Borowski <moderator9781@gmail.com>
2023-09-26 17:23:42 +0200

    Add product detail view

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,5 +1,24 @@
-function getAllProducts(req, res) {
-  res.render("customer/products/all-products");
+const Product = require("../models/product.model");
+
+async function getAllProducts(req, res, next) {
+  try {
+    const products = await Product.findAll();
+    res.render("customer/products/all-products", {
+      products: products,
+    });
+  } catch (error) {
+    next(error);
+    return;
+  }
 }
 
-module.exports = { getAllProducts: getAllProducts };
+async function getProduct(req, res, next) {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render("customer/products/product-details", { product: product });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { getAllProducts: getAllProducts, getProduct: getProduct };
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.get("/products", productsController.getAllProducts)
 
+router.get("/products/:id", productsController.getProduct)
+
 module.exports = router;
